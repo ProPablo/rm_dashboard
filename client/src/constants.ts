bf333d79a591e42d090b4d01ca15eb296a754bb9
Anhad <anhadrs@gmail.com>
2021-04-10 08:47:02 +1000

    Media constants

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,4 +1,4 @@
 export const SERVER_URL = process.env.NODE_ENV === "production" ? "https://rm.kongroo.xyz/api" : "http://localhost:3001";
 
-export const IMAGES_URL = process.env.NODE_ENV === "production" ? "" : SERVER_URL;
+export const MEDIA_URL = process.env.NODE_ENV === "production" ? "" : SERVER_URL;
 // export const IMAGES_URL = process.env.NODE_ENV === "production" ? `${window.location.origin}` : SERVER_URL;
