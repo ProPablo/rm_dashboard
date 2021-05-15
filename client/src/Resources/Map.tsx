3fd68337d01f90c4e3e0e90725b945642c2cebb5
ProPablo <40000658+ProPablo@users.noreply.github.com>
2021-05-15 14:23:12 +1000

    working map dragging

--- client/src/Resources/Map.tsx
+++ client/src/Resources/Map.tsx
@@ -1,24 +1,48 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { Title } from 'react-admin'
-import { useDrag } from 'react-use-gesture'
-
+import { useDrag, useGesture } from 'react-use-gesture'
+import floorPlan from '../floorplan.jpg';
 
 export interface MapProps {
 
 }
 
 export const Map = (props: MapProps) => {
-  const [{ x, y }, setState] = useState({ x: 0, y: 0 });
+  const [{ xOffset, yOffset }, setOffset] = useState({ xOffset: 0, yOffset: 0 });
+  const [zoom, setZoom] = useState(1);
+  const mapRef = useRef(null);
 
-  const bind = useDrag(({ down, movement: [mx, my] }) => {
-    setState({ x: down ? mx : 0, y: down ? my : 0 })
-    console.log({ x, y });
-  })
+  const bind = useGesture(
+    {
+      onDrag: ({ down, movement: [mx, my] }) => {
+        setOffset({ xOffset: down ? mx : 0, yOffset: down ? my : 0 });
+      },
+      onWheel: (state) => {
+        // state.movement.y;
+        console.log({ ...state });
+      }
+    }
+  )
 
+  const style = {
+    transform: `scale(${zoom})`,
+    top: `${yOffset}px`,
+    left: `${xOffset}px`
+  };
   return (
     <div className="map-container">
       <Title title="Map" />
-      <div {...bind()} className="draggable" style={{transform: `translate(${x}px, ${y}px)`}}/>
+      <div
+        className="movable map-main"
+
+      >
+        <img
+          style={style}
+          {...bind()}
+          src={floorPlan}
+          draggable="false"
+          className="map-image" />
+      </div>
     </div>
   )
 }
