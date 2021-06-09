5142fad76a7c3b6c43cdd4827a903d6157108394
Anhad <anhadrs@gmail.com>
2021-06-09 22:42:15 +1000

    Readme and controller fix

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Redland Mueseum Guest Interaction System
+# Redland Mueseum Display Information and Guidance System (DIGS)
 
 An administration management portal and app for the Redland Museum Display Information and Guidance System created with the React Admin framework. This project is created as a part of the IFB399 CAPSTONE PHASE 2 subject at Queensland University of Technology (QUT). 
 Run server and client together for development and assign appropriate dotENV. Deployment occurs live by compiling every push to master using GitHub actions. RM_dashboard communicates between Codebase2, the complementary React Native application. Swagger documentation can be accessed by either appending `/api/swagger` to the base `rm.kongroo.xyz` url or `/swagger` to the localhost development server. 
--- app/lib/controllers.ts
+++ app/lib/controllers.ts
@@ -1,6 +1,6 @@
 import { backendURL } from "../env.js";
 
-export const baseURL = (process.env.NODE_ENV === "production") ? "https://rm.kongroo.xyz/api" : backendURL;
+export const baseURL = process.env.NODE_ENV === "production" ? "https://rm.kongroo.xyz/api" : backendURL;
 
 export const MEDIA_URL = process.env.NODE_ENV === "production" ? "https://rm.kongroo.xyz" : baseURL;
 
@@ -8,9 +8,13 @@ export const MEDIA_URL = process.env.NODE_ENV === "production" ? "https://rm.kon
 export function request<T>(url: string): Promise<T> {
   return fetch(url).then(response => {
     if (!response.ok) {
-      //TODO: fix if nginx error or if server not found
-      response.json().then(body => { throw new Error(body.message) })
+      response.json().then(body => {
+        throw new Error(body.message)
+      }).catch((err) => {
+        throw new Error("Server not found or no connection");
+      })
     }
     return response.json() as Promise<T>
   })
+    .catch(e => { throw e })
 }
